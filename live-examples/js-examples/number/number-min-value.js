c05ce40de906f915cbe4618b63d79ff6f675438b
Les Potter <lesterpotter@users.noreply.github.com>
2020-08-22 08:20:27 -0400

    Fix example
    Fix example to demonstrate Number.MIN_VALUE instead of Number.NEGATIVE_INFINITY

--- live-examples/js-examples/number/number-min-value.js
+++ live-examples/js-examples/number/number-min-value.js
@@ -8,5 +8,8 @@ function multiply(x, y) {
 console.log(multiply(5e-324, 1));
 // expected output: 5e-324
 
-console.log(multiply(-1.7976931348623157e+308, 2));
-// expected output: Process as -Infinity
+console.log(multiply(58-324, .5));
+// expected output: 0
+
+console.log(multiply(58-324, .6));
+// expected output: 5e-324
